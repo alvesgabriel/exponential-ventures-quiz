65b1a85f2abb9f269cf6f747322d45254d4f42fd
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2020-11-15 10:11:02 -0300

    :sparkles: Created navbar
    close #7

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 class Home extends React.Component {
   render() {
     var user = JSON.parse(localStorage.getItem("user"));
-    return <h1>Hello {user.name}</h1>;
+
+    return (
+      <div>
+        <Navbar />
+        <h1>Hello {user.name}</h1>
+      </div>
+    );
   }
 }
 
